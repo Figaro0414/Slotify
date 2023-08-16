51790d8df7b216996e88150f8659d5a207a64674
Jonathan <jonathan.moya0414@gmail.com>
2023-08-16 17:29:50 -0400

    second time is the charm?

--- backend/song_app/urls.py
+++ backend/song_app/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import All_songs
+from .views import All_songs, A_song
 # from .views import 
 urlpatterns=[
-    path('', All_songs.as_view(), name='songs')
+    path('', All_songs.as_view(), name='songs'),
+    path('<int:id_or_name>/', A_song.as_view(), name="a_song")
 ]
